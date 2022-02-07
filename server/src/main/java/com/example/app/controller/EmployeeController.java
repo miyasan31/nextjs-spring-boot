44887090b1aceb8c11dd8dd2ade412ed5fc5aa25
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-07 18:23:36 +0900

    ✨ /api/employee - CURD

--- server/src/main/java/com/example/app/controller/EmployeeController.java
+++ server/src/main/java/com/example/app/controller/EmployeeController.java
@@ -3,14 +3,20 @@ package com.example.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import com.example.app.entity.Employee;
 import com.example.app.repository.EmployeeRepository;
 
 @RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
   
   private final EmployeeRepository employeeRepository;
@@ -21,10 +27,40 @@ public class EmployeeController {
   }
 
 
-  @GetMapping("/employee")
-  public Iterable<Employee> employee() {
-    // CrudRepository の メソッドが使える
+  @GetMapping()
+  @ResponseStatus(HttpStatus.OK)
+  public Iterable<Employee> getEmployeeList() {
     return employeeRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Employee getEmployee(@PathVariable Long id) {
+    return employeeRepository.findById(id).get();
+  }
+
+  @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
+  public void postEmployee(
+    @RequestBody Employee employee
+  ) {
+    employeeRepository.save(employee);
+  }
+
+  @PatchMapping()
+  @ResponseStatus(HttpStatus.OK)
+  public void putEmployee(
+    @RequestBody Employee employee
+  ) {
+    employeeRepository.save(employee);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteEmployee(
+    @PathVariable Long id
+  ) {
+    employeeRepository.deleteById(id);
   }
 
 }
