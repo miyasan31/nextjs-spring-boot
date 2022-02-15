43bf701b29fe2ad9f40af0feb7a82cd8634d16d7
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-15 17:23:24 +0900

    🔧 styletron

---
+++ client/src/util/styletron.ts
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import { Client, Server } from "styletron-engine-atomic";
+
+const getHydrateClass = () =>
+  document.getElementsByClassName("_styletron_hydrate_") as HTMLCollectionOf<HTMLStyleElement>;
+
+export const styletron =
+  typeof window === "undefined"
+    ? new Server()
+    : new Client({
+        hydrate: getHydrateClass(),
+      });
