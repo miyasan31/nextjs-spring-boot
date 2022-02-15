56f7dc56719a735ce3934953cc74bf3581a56526
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-16 02:24:32 +0900

    ✨ RootPage

--- client/src/component/page/Root/Root.page.tsx
+++ client/src/component/page/Root/Root.page.tsx
@@ -2,14 +2,11 @@ import type { VFC } from "react";
 
 import { LayoutErrorBoundary } from "~/component/functional/ErrorBoundary";
 import { Root } from "~/component/page/Root/Root";
-import { Layout } from "~/component/ui/Layout";
 
 export const RootPage: VFC = () => {
   return (
-    <Layout>
-      <LayoutErrorBoundary>
-        <Root />
-      </LayoutErrorBoundary>
-    </Layout>
+    <LayoutErrorBoundary>
+      <Root />
+    </LayoutErrorBoundary>
   );
 };
--- client/src/component/page/Root/Root.tsx
+++ client/src/component/page/Root/Root.tsx
@@ -1,24 +1,109 @@
-import { Button } from "baseui/button";
-import { ButtonGroup } from "baseui/button-group";
-import { StatefulInput } from "baseui/input";
+/* eslint-disable @typescript-eslint/naming-convention */
+import { styled, withStyle } from "baseui";
+import { Button, StyledBaseButton } from "baseui/button";
+import { Combobox } from "baseui/combobox";
+import { FormControl } from "baseui/form-control";
+import { format } from "date-fns";
 import type { VFC } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useStyletron } from "styletron-react";
 
+import { BoxLayout } from "~/component/ui/Layout";
 import { useGetSWR } from "~/hook";
+import type { Employee } from "~/type/model";
 
 export const Root: VFC = () => {
-  const { isLoading, isError, data } = useGetSWR("/api/employee");
-  console.info("isLoading", isLoading);
-  console.info("isError", isError);
-  console.info("data", data);
+  const { isLoading, isError, data } = useGetSWR<Employee[]>("/api/employee");
+
+  const [css] = useStyletron();
+  const space = css({ marginBottom: "20px" });
+  const [value, setValue] = useState<string>("");
+  const [time, setTime] = useState(new Date());
+
+  const handleSelect = useCallback((value) => {
+    setValue(value);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      return setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error</div>;
+  }
+  if (!data) {
+    return <div>Not found</div>;
+  }
+
+  const clock = format(time, "HH:mm:ss");
+  const result = data.map((employee: Employee) => {
+    return { id: employee.id, label: `${employee.first_name} ${employee.last_name}` };
+  });
 
   return (
-    <div>
-      <StatefulInput />
-      <ButtonGroup>
-        <Button isLoading>Button 1</Button>
-        <Button>Button 2</Button>
-        <Button>Button 3</Button>
-      </ButtonGroup>
-    </div>
+    <BoxLayout title="出勤登録">
+      <Clock>{clock}</Clock>
+
+      <FormRoot>
+        <FormControl label="従業員を選択してください">
+          <Combobox
+            value={value}
+            onChange={handleSelect}
+            options={result}
+            mapOptionToString={(option) => {
+              return option.label;
+            }}
+          />
+        </FormControl>
+
+        <div className={space} />
+
+        <Button overrides={{ BaseButton: CustomeButton }}>出勤する</Button>
+      </FormRoot>
+    </BoxLayout>
   );
 };
+
+const Clock = styled("h1", ({ $theme }) => {
+  return {
+    margin: 0,
+    paddingTop: `${$theme.sizing.scale1600}`,
+
+    textAlign: "center",
+    fontSize: `${$theme.sizing.scale1600}`,
+    lineHeight: `${$theme.sizing.scale1400}`,
+  };
+});
+
+const FormRoot = styled("div", ({ $theme }) => {
+  return {
+    padding: `${$theme.sizing.scale1000}`,
+
+    border: `1px solid ${$theme.colors.primary100}`,
+    borderTop: 0,
+
+    fontSize: `${$theme.sizing.scale800}`,
+    lineHeight: `${$theme.sizing.scale1400}`,
+  };
+});
+
+const CustomeButton = withStyle(StyledBaseButton, ({ $theme }) => {
+  return {
+    width: "100%",
+
+    color: $theme.colors.primary100,
+    backgroundColor: $theme.colors.backgroundInverseSecondary,
+
+    ":hover": {
+      backgroundColor: $theme.colors.backgroundInversePrimary,
+    },
+  };
+});
